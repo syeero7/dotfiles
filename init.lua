2911da9ac68205df59293e1a5eafd88dc7856422
emal <emalhasitha@proton.me>
2026-05-13 08:40:02 +0530

    feat: add auto-commands

--- init.lua
+++ init.lua
@@ -2,3 +2,4 @@ vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
 require("config.options")
+require("config.autocmds")
---
+++ lua/config/autocmds.lua
@@ -0,0 +1,54 @@
+local function augroup(name)
+	return vim.api.nvim_create_augroup("user_" .. name, { clear = true })
+end
+
+vim.api.nvim_create_autocmd('TextYankPost', {
+  desc = 'Highlight when yanking (copying) text',
+  group = augroup("highlight_yank"),
+  callback = function()
+    vim.hl.on_yank()
+  end,
+})
+
+vim.api.nvim_create_autocmd("BufReadPost", {
+	group = augroup("last_loc"),
+	callback = function(event)
+		local exclude = { "gitcommit" }
+		local buf = event.buf
+		if vim.tbl_contains(exclude, vim.bo[buf].filetype) or vim.b[buf].lazyvim_last_loc then
+			return
+		end
+		vim.b[buf].lazyvim_last_loc = true
+		local mark = vim.api.nvim_buf_get_mark(buf, '"')
+		local lcount = vim.api.nvim_buf_line_count(buf)
+		if mark[1] > 0 and mark[1] <= lcount then
+			pcall(vim.api.nvim_win_set_cursor, 0, mark)
+		end
+	end,
+})
+
+vim.api.nvim_create_autocmd("InsertEnter", {
+	group = augroup("insert_ui_perf"),
+	callback = function()
+		vim.wo.cursorline = false
+		vim.wo.relativenumber = false
+		vim.wo.number = true -- keep absolute numbers
+	end,
+})
+
+vim.api.nvim_create_autocmd("InsertLeave", {
+	group = augroup("insert_ui_perf"),
+	callback = function()
+		vim.wo.cursorline = true
+		vim.wo.relativenumber = true
+	end,
+})
+
+-- make it easier to close man-files when opened inline
+vim.api.nvim_create_autocmd("FileType", {
+	group = augroup("man_unlisted"),
+	pattern = { "man" },
+	callback = function(event)
+		vim.bo[event.buf].buflisted = false
+	end,
+})
